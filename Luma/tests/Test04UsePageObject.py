8e2dea1c1deb4aefa8b53a732ccf37bcbe59d252
Oleksandr Burenok <oburenok@softserveinc.com>
2024-10-12 20:15:25 +0300

    Refactoring page object (replacing BasePage by HomePage). Adding two test methods to verify pytest mark XFAIL.

--- Luma/tests/Test04UsePageObject.py
+++ Luma/tests/Test04UsePageObject.py
@@ -5,16 +5,18 @@ This is a first test for commercial website Luma.
 import time
 import pytest
 
-from pages.main.base import BasePage
+from pages.home_page.home_page import HomePage
 from utils import log
+
+# pytestmark = pytest.mark.skipif(pytest.__version__ != '7.3.2', reason='Pytest version is less then 7.3.2')
 
 
 @pytest.mark.usefixtures("driver")
 class Test04UsePageObject:
 
-    @pytest.mark.sanity
+    # @pytest.mark.integration
     def test_01_use_base_page_object(self):
-        self.home_page = BasePage(self.driver, "https://magento.softwaretestingboard.com/what-is-new.html")
+        self.home_page = HomePage(self.driver, self.url)
 
         log.message("Search product.")
         self.home_page.enter_text_in_search_field("Tank")
@@ -22,7 +24,7 @@ class Test04UsePageObject:
 
         time.sleep(2)
 
-    # @pytest.mark.skip()
+    @pytest.mark.skip(reason="Test contains to much warnings and critical messages.")
     def test_02_navigation_to_google(self):
         self.driver.get("https://www.google.com.ua")
         log.message("bla-bla-bla")
@@ -36,6 +38,18 @@ class Test04UsePageObject:
         log.message("Add more comments")
         log.message("to simulate testing process.")
 
-        # log.critical("Log critical to increase critical counter.")
-        # time.sleep(1.5)
+        log.critical("Log critical to increase critical counter.")
+        time.sleep(1.5)
+
+    @pytest.mark.xfail
+    def test_03_failed(self):
+        log.message("Test should pass.")
+        assert 1 == 0
+
+    @pytest.mark.xfail
+    def test_04_failed_pass(self):
+        log.message("Test should fail.")
+        assert 1 == 1
+
+
 
