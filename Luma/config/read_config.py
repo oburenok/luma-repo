2d516e16a28e2316329ecfaa7c2bf3a44de82982
Oleksandr Burenok <oburenok@softserveinc.com>
2024-10-15 17:46:40 +0300

    Removing extra comments.

--- Luma/config/read_config.py
+++ Luma/config/read_config.py
@@ -15,7 +15,6 @@ class GetConfig:
     """
 
     def __init__(self):
-        # globl.project_name = 'Luma'  # This is constant variable
         print("============ os.getcwd() returns this: " + os.getcwd())
         self.get_project_path(os.getcwd())
         self.config = configparser.ConfigParser()
