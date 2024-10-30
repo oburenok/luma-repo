d2e541187cb15d4f3f0590515164c469d6d86152
Oleksandr Burenok <oburenok@softserveinc.com>
2024-10-30 23:11:08 +0200

    Adding test method, where test data taken from XLSX file.

--- Luma/tests/Test07_ReadFromFileAndSearch.py
+++ Luma/tests/Test07_ReadFromFileAndSearch.py
@@ -11,10 +11,6 @@ from utils import log, read_data
 @pytest.mark.usefixtures("driver")
 class Test07ReadFromFileAndSearch:
 
-    # def test_00_get_test_data(self):
-    #     self.test_data = read_data.excel_to_dict('Test07_ReadFromFileAndSearch.xlsx',
-    #                                              ['SearchResults', 'HomePage'])
-
     @pytest.mark.parametrize("test_data", read_data.csv_to_dict("Test07_ReadFromFileAndSearch.csv"))
     def test_01_read_csv_and_search(self, test_data):
 
@@ -24,6 +20,18 @@ class Test07ReadFromFileAndSearch:
         self.results_page.enter_text_in_search_field(test_data["search_product"])
         self.results_page.click_search()
 
-        log.message("Verify number of products.")
+        log.message("Verify number of products on the page.")
+        self.results_page.count_products(test_data["found_products_number"])
+
+    @pytest.mark.parametrize("test_data", read_data.excel_to_dict('Test07_ReadFromFileAndSearch.xlsx', 'SearchResults'))
+    def test_02_read_xlsx_and_search(self, test_data):
+
+        self.results_page = ResultsPage(self.driver, self.url)
+
+        log.message("Search product.")
+        self.results_page.enter_text_in_search_field(test_data["search_product"])
+        self.results_page.click_search()
+
+        log.message("Verify number of products on the page.")
         self.results_page.count_products(test_data["found_products_number"])
 
