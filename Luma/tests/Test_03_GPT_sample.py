e3818b8c60f4cb9fd1c148805ae6e211b63fb63b
Oleksandr Burenok <oburenok@softserveinc.com>
2024-10-31 22:47:25 +0200

    Adding Debug messages in test.

--- Luma/tests/Test_03_GPT_sample.py
+++ Luma/tests/Test_03_GPT_sample.py
@@ -16,11 +16,13 @@ class Test03GPTsample:
         log.screenshot("bla-bla-bla")
         log.message("bla-bla-bla")
         log.message("Some comments")
+        log.debug_message("This debug message.")
 
         log.error("Log error to increase error counter.")
         assert True
 
     def test_example_2(self):
+        log.debug_message("Debugging test.")
         self.driver.get("https://www.google.com.ua")
         log.warning("It is warning message.")
         time.sleep(1.5)
