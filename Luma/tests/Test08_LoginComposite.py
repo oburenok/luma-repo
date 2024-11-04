c1ed1c0e3e2ee7a3648111db9db2854a0d1821ac
Oleksandr Burenok <oburenok@softserveinc.com>
2024-11-04 23:38:42 +0200

    Adding test for Login screen (Composite Pattern).

---
+++ Luma/tests/Test08_LoginComposite.py
@@ -0,0 +1,29 @@
+"""
+This is a Login test for commercial website Luma.
+"""
+
+import pytest, time
+
+from pages.results_page.results_page import ResultsPage
+from pages.login_page.login_page import LoginPage
+from utils import log
+
+
+@pytest.mark.usefixtures("driver")
+class Test08LoginComposite:
+
+    def test_01_logging(self):
+
+        self.results_page = ResultsPage(self.driver, self.url)
+        self.loginPage = LoginPage(self.driver, self.url)
+
+        log.message("Logging to the site Luma and verify error.")
+        self.loginPage.load()
+        self.loginPage.login_and_verify_error("invalid@gmail.com", "qwerty12345",
+                                              "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.")
+        time.sleep(1.5)
+
+        log.message("Logging to the site Luma.")
+        self.loginPage.load()
+        self.loginPage.login("User1@gmail.com", "qwerty12345")
+
