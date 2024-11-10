6dcd06fc30529168b6e31fcd2ffc7301201e919b
Oleksandr Burenok <oburenok@softserveinc.com>
2024-11-10 17:32:12 +0200

    Adding new method utils/verify.py->is_not_equal().

--- Luma/utils/verify.py
+++ Luma/utils/verify.py
@@ -30,3 +30,33 @@ def is_equal(a, b, msg=None,):
     except Exception as exc:
         log.exception(str(exc))
 
+
+def is_not_equal(a, b, msg=None,):
+    """
+    This function verifies that a and b are NOT equal.
+
+    .. function:: is_not_equal
+
+    :param a: Actual value
+    :type a: var
+    :param b: Value to compare with
+    :type b: var
+    :param msg: Message passed to assert NOT Equal
+    :type msg: str
+
+    Examples:
+            s = "hello world"
+            verify.equal(1, 0, "Values should not be equal.")
+    """
+    try:
+        globl.test_counters['total_checkpoints'] += 1
+        log.message(str(msg))
+
+        if a != b:
+            log.message(f"Value {a} is not equal to value {b}.")
+        else:
+            log.warning(f"Value {a} is EQUAL to the value {b}.")
+
+    except Exception as exc:
+        log.exception(str(exc))
+
