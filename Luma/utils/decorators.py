a11368a9c1d16151db153a3836003b93584176e5
Oleksandr Burenok <oburenok@softserveinc.com>
2024-11-27 22:52:05 +0200

    Refactoring module decorators.py according to pylint.

--- Luma/utils/decorators.py
+++ Luma/utils/decorators.py
@@ -1,15 +1,22 @@
+"""
+This module contains all decorators for test framework.
+"""
 import time
-from functools import wraps
+
 from utils import log
 
 
 def stopwatch(func):
+    """
+    This decorator can be used to measure the time.
+    :param func: function to be decorated
+    :return: decorated function
+    """
     def wrapper(*args, **kwargs):
         start_time = time.time()
 
         func(*args, **kwargs)
 
         run_time = time.time() - start_time
-        log.message(f"Test run time is: {run_time} seconds")
+        log.message(f"Test runtime is: {run_time} seconds")
     return wrapper
-
