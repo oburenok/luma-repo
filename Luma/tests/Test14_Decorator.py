cc3227eddf3b84baaefe825ff8d4a26caadc43dc
Oleksandr Burenok <oburenok@softserveinc.com>
2024-11-28 19:05:03 +0200

    Adding constant and global variable in test tests/Test14_Decorator.py.

--- Luma/tests/Test14_Decorator.py
+++ Luma/tests/Test14_Decorator.py
@@ -1,16 +1,30 @@
+"""
+Test for decorator.
+"""
 import pytest
 
 from pages.cart_page.cart_page import CartPage
 from utils import log, decorators
 
 
+STRING_VARIABLE = "Test string for global variable."
+
+
 @pytest.mark.usefixtures("driver")
 class Test14Decorator:
+    """Testing decorator 'stopwatch'."""
 
     @decorators.stopwatch
     def test_01_decorator(self):
+        """Testing decorator 'stopwatch'."""
+        global STRING_VARIABLE
+
         self.cartPage = CartPage(self.driver, self.url)
 
         log.message("Navigate to Cart page.")
         self.cartPage.load()
 
+        log.message(STRING_VARIABLE)
+
+        STRING_VARIABLE = "String is updated."
+        log.message(STRING_VARIABLE)
