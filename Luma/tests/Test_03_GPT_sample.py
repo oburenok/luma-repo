de47940aa3354266e003c2182f82699ff38d2a41
Oleksandr Burenok <oburenok@softserveinc.com>
2024-12-19 22:14:18 +0200

    Refactoring test tests/Test_03_GPT_sample.py according to new test run approach.

--- Luma/tests/Test_03_GPT_sample.py
+++ Luma/tests/Test_03_GPT_sample.py
@@ -3,28 +3,25 @@ This is a first test for commercial website Luma.
 """
 
 import pytest
-import time
 
 from utils import log
 
 pytestmark = [pytest.mark.run_every_night]
 
 
-@pytest.mark.usefixtures("driver")
-class Test03GPTsample:
+def test_example_1(driver):
+    driver.get("https://magento.softwaretestingboard.com/what-is-new.html")
+    log.screenshot("bla-bla-bla")
+    log.message("bla-bla-bla")
+    log.message("Some comments")
+    log.debug_message("This debug message.")
 
-    def test_example_1(self):
-        self.driver.get("https://magento.softwaretestingboard.com/what-is-new.html")
-        log.screenshot("bla-bla-bla")
-        log.message("bla-bla-bla")
-        log.message("Some comments")
-        log.debug_message("This debug message.")
+    log.error("Log error to increase error counter.")
+    assert True
 
-        log.error("Log error to increase error counter.")
-        assert True
 
-    def test_example_2(self):
-        log.debug_message("Debugging test.")
-        self.driver.get("https://www.google.com.ua")
-        log.warning("It is warning message.")
+def test_example_2(driver):
+    log.debug_message("Debugging test.")
+    driver.get("https://www.google.com.ua")
+    log.warning("It is warning message.")
 
