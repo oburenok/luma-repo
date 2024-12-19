385176f8f692f064ee454e5b527011c5b7fc414a
Oleksandr Burenok <oburenok@softserveinc.com>
2024-12-19 22:29:04 +0200

    Refactoring tests/Test_05_UsePageObject.py according to new test run approach.

--- Luma/tests/Test_05_UsePageObject.py
+++ Luma/tests/Test_05_UsePageObject.py
@@ -6,41 +6,39 @@ import pytest
 
 from pages.home_page.home_page import HomePage
 from pages.results_page.results_page import ResultsPage
-from utils import log
+from utils import log, globl
 
 pytestmark = [pytest.mark.integration, pytest.mark.sanity]
 
 
-@pytest.mark.usefixtures("driver")
-class Test05UsePageObject:
+@pytest.mark.sanity
+def test_01_use_base_page_object(driver):
+    home_page = HomePage(driver, globl.url)
 
-    @pytest.mark.sanity
-    def test_01_use_base_page_object(self):
-        self.home_page = HomePage(self.driver, self.url)
+    log.message("Search product.")
+    home_page.enter_text_in_search_field("Bag")
+    home_page.click_search()
 
-        log.message("Search product.")
-        self.home_page.enter_text_in_search_field("Bag")
-        self.home_page.click_search()
+    log.message("Navigate to category menus.")
+    home_page.category_menu.navigate_to_category("woman wtops wjackets")
 
-        log.message("Navigate to category menus.")
-        self.home_page.category_menu.navigate_to_category("woman wtops wjackets")
+    home_page.category_menu.navigate_to_category("man mtops mtees", click=False)
 
-        self.home_page.category_menu.navigate_to_category("man mtops mtees", click=False)
+    home_page.category_menu.navigate_to_category("gear fitnessequipment")
 
-        self.home_page.category_menu.navigate_to_category("gear fitnessequipment")
 
-    @pytest.mark.smoke
-    def test_02_navigation_to_google(self):
-        self.results_page = ResultsPage(self.driver, self.url)
+@pytest.mark.smoke
+def test_02_navigation_to_google(driver):
+    results_page = ResultsPage(driver, globl.url)
 
-        log.message("Search product.")
-        self.results_page.enter_text_in_search_field("Watch")
-        self.results_page.click_search()
+    log.message("Search product.")
+    results_page.enter_text_in_search_field("Watch")
+    results_page.click_search()
 
-        log.step("1.001", "Verify product.")
-        self.results_page.verify_product()
+    log.step("1.001", "Verify product.")
+    results_page.verify_product()
 
-        log.step("1.002", "Count products on results page.")
-        self.results_page.count_products(5)
+    log.step("1.002", "Count products on results page.")
+    results_page.count_products('9')
 
 
