83c7e107440e955042573dbfcc1568eca1a67bfd
Oleksandr Burenok <oburenok@softserveinc.com>
2024-12-19 22:34:46 +0200

    Refactoring tests/Test_06_ParametrizeSearch.py according to new test run approach.

--- Luma/tests/Test_06_ParametrizeSearch.py
+++ Luma/tests/Test_06_ParametrizeSearch.py
@@ -5,24 +5,21 @@ This is a first test for commercial website Luma.
 import pytest
 
 from pages.results_page.results_page import ResultsPage
-from utils import log
+from utils import log, globl
 
 pytestmark = [pytest.mark.run_every_night, pytest.mark.integration]
 
 
-@pytest.mark.usefixtures("driver")
-class Test06ParametrizeSearch:
+@pytest.mark.parametrize('product, counter', [('bag', '10'), ('tshirt', '0'), ('watch', '9')])
+def test_01_use_base_page_object(driver, product, counter):
+    results_page = ResultsPage(driver, globl.url)
 
-    @pytest.mark.parametrize('product, counter', [('bag', '10'), ('tshirt', '0'), ('watch', '9')])
-    def test_01_use_base_page_object(self, product, counter):
-        self.results_page = ResultsPage(self.driver, self.url)
+    log.message("Search product.")
+    results_page.enter_text_in_search_field(product)
+    results_page.click_search()
 
-        log.message("Search product.")
-        self.results_page.enter_text_in_search_field(product)
-        self.results_page.click_search()
+    log.message("Verify number of products.")
+    results_page.count_products(counter)
 
-        log.message("Verify number of products.")
-        self.results_page.count_products(counter)
-
-        log.message("Update to make Jenkins start build after this commit.")
+    log.message("Update to make Jenkins start build after this commit.")
 
