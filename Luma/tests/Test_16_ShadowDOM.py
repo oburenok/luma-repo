6b110c64539ff3ca767728a79b1e8e44e8d05eff
Oleksandr Burenok <oburenok@softserveinc.com>
2024-12-19 23:42:58 +0200

    Refactoring tests/Test_16_ShadowDOM.py according to new test run approach.

--- Luma/tests/Test_16_ShadowDOM.py
+++ Luma/tests/Test_16_ShadowDOM.py
@@ -1,41 +1,37 @@
 """
-This is more experimental test for Shadow DOM elements, and it is not actually related to Test Framework.
+This is EXPERIMENTAL TEST for Shadow DOM elements, and it is not actually related to Test Framework.
 """
 import pytest
-import time
 
 from selenium.webdriver.common.by import By
 from pages.product_page.product_page import ProductPage
-from utils import log
+from utils import log, globl
 
 pytestmark = [pytest.mark.integration]
 
 
-@pytest.mark.usefixtures("driver")
-class Test16ShadowDOM:
+def test_01_verify_default_product_info(driver):
 
-    def test_01_verify_default_product_info(self):
+    log.message("Navigate to tested webpage.")
+    web_page = "https://letcode.in/shadow"
+    product_page = ProductPage(driver, globl.url)
+    product_page.navigate_to_page(web_page, close_footer_ads=False)
 
-        log.message("Navigate to tested webpage.")
-        web_page = "https://letcode.in/shadow"
-        self.productPage = ProductPage(self.driver, self.url)
-        self.productPage.navigate_to_page(web_page, close_footer_ads=False)
+    log.message("Enter text in the field in Shadow DOM (open).")
+    shadow_host = driver.find_element(By.XPATH, "//div[@id='open-shadow']")
+    elem = shadow_host.shadow_root.find_element(By.CSS_SELECTOR, 'input[id="fname"]')
+    elem.send_keys("my name")
 
-        log.message("Enter text in the field in Shadow DOM (open).")
-        shadow_host = self.driver.find_element(By.XPATH, "//div[@id='open-shadow']")
-        elem = shadow_host.shadow_root.find_element(By.CSS_SELECTOR, 'input[id="fname"]')
-        elem.send_keys("my name")
+    log.message("Enter text in the field in Shadow DOM (closed). Reopen Shadow DOM by CDP")
+    driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {'source': """
+    Element.prototype._attachShadow = Element.prototype.attachShadow;
+    Element.prototype.attachShadow = function () {
+        return this._attachShadow( { mode: "open" } );
+    };
+    """})
+    driver.get(web_page)
 
-        log.message("Enter text in the field in Shadow DOM (closed). Reopen Shadow DOM by CDP")
-        self.driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {'source': """
-        Element.prototype._attachShadow = Element.prototype.attachShadow;
-        Element.prototype.attachShadow = function () {
-            return this._attachShadow( { mode: "open" } );
-        };
-        """})
-        self.driver.get(web_page)
-
-        shadow_host = self.driver.find_element(By.XPATH, "//div[@id='close-shadow']")
-        elem = shadow_host.shadow_root.find_element(By.CSS_SELECTOR, 'input[id="email"]')
-        elem.send_keys("my email")
+    shadow_host = driver.find_element(By.XPATH, "//div[@id='close-shadow']")
+    elem = shadow_host.shadow_root.find_element(By.CSS_SELECTOR, 'input[id="email"]')
+    elem.send_keys("my email")
 
