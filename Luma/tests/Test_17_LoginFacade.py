763771ed25bb0c448359ac6cfb274ccc8d0a25a4
Oleksandr Burenok <oburenok@softserveinc.com>
2024-12-19 23:47:49 +0200

    Refactoring tests/Test_17_LoginFacade.py according to new test run approach.

--- Luma/tests/Test_17_LoginFacade.py
+++ Luma/tests/Test_17_LoginFacade.py
@@ -4,25 +4,23 @@ This is a Login test uses Login Facade.
 import pytest
 
 from pages.login_page.login_facade import LoginFacade
-from utils import log
+from utils import log, globl
 
 pytestmark = [pytest.mark.run_every_night]
 
 
-@pytest.mark.usefixtures("driver")
-class Test17LoginFacade:
+def test_01_logging(driver):
+    login_facade = LoginFacade(driver, globl.url)
 
-    def test_01_logging(self):
-        self.login_facade = LoginFacade(self.driver, self.url)
+    log.step("1.001", "Naigate to Login page.")
+    login_facade.load()
 
-        log.step("1.001", "Naigate to Login page.")
-        self.login_facade.load()
+    log.message("Logging to the site Luma and verify error.")
+    login_facade.login_and_verify_error("invalid@gmail.com", "qwerty12345",
+                                        "The account sign-in was incorrect or your account is disabled temporarily. "
+                                        "Please wait and try again later.")
 
-        log.message("Logging to the site Luma and verify error.")
-        self.login_facade.login_and_verify_error("invalid@gmail.com", "qwerty12345",
-                                              "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.")
-
-        log.step("1.002", "Logging to the site Luma.")
-        self.login_facade.load()
-        self.login_facade.login("User1@gmail.com", "qwerty12345")
+    log.step("1.002", "Logging to the site Luma.")
+    login_facade.load()
+    login_facade.login("User1@gmail.com", "qwerty12345")
 
